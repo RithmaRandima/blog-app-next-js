eb6973ea71da05415e2aa51becb81cdf24a6ed2b
RithmaRandima <rithma.r.nandasiri@gmail.com>
2025-11-02 12:14:40 +0530

    Completed blogitem component

--- Assets/assets.js
+++ Assets/assets.js
@@ -1,15 +1,98 @@
-import blog_img_1 from "./blog_img_1.jpg";
+import blog_img_1 from "./blog_pic_1.jpg";
+import profile_icon from "./profile_pic.jpeg";
 
 export const assets = {
   blog_img_1,
+  profile_icon,
 };
 
 export const blog_data = [
   {
     id: 1,
-    title: "",
-    descrption: "",
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
     image: blog_img_1,
     date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 2,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 3,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 4,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 5,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 6,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 7,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
+  },
+  {
+    id: 8,
+    title: "A detailed step by step guid to manage your lifestyle",
+    descrption:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, autem",
+    image: blog_img_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon,
   },
 ];
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
+"use client";
+import BlogItem from "@/Components/BlogItem";
 import Header from "@/Components/Header";
 import Image from "next/image";
-import { AiFillAccountBook } from "react-icons/ai";
 
 export default function Home() {
   return (
     <>
       <h1>
         <Header />
+        <BlogItem />
       </h1>
     </>
   );
