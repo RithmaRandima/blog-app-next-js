f5df381ec8b4ce44db23fb4528212ff58eaf2afa
RithmaRandima <rithma.r.nandasiri@gmail.com>
2025-11-11 10:19:49 +0530

    completed Route in Admin Sidebar component

---
+++ Components/adminComponents/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { IoAddCircleSharp } from "react-icons/io5";
+import { IoMdListBox } from "react-icons/io";
+import { RiMailCheckFill } from "react-icons/ri";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col bg-slate-100 h-[100vh] border border-black px-2 sm:px-0">
+      <div className="px-2 py-2 ">
+        <h1 className="font-bold text-2xl">Blogger</h1>
+      </div>
+      <div className="w-40 sm:w-70 relative py-10 flex flex-col gap-4 ">
+        <div className="w-[100%] sm:w-[70%] absolute right-0">
+          <Link
+            href="/admin/addProduct"
+            className="flex items-center border border-black gap-3 font-medium my-3 px-3 py-2 bg-white shadow-[-5px_5px_0px_#000]"
+          >
+            <IoAddCircleSharp className="text-2xl" />
+            <p className="font-semibold">Add Blogs</p>
+          </Link>
+
+          <Link
+            href="/admin/blogList"
+            className="flex items-center border border-black gap-3 font-medium my-3 px-3 py-2 bg-white shadow-[-5px_5px_0px_#000]"
+          >
+            <IoMdListBox className="text-2xl" />
+            <p className="font-semibold">Blog List</p>
+          </Link>
+
+          <Link
+            href="/admin/subscription"
+            className="flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white my-3 shadow-[-5px_5px_0px_#000]"
+          >
+            <RiMailCheckFill className="text-2xl" />
+            <p className="font-semibold">Subscriptions</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ app/admin/addProduct/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddProduct = () => {
+  return (
+    <div className="p-5">
+      <h1 className="text-2xl font-bold">Add Blog Page</h1>
+      <p>This is your add blog form.</p>
+    </div>
+  );
+};
+
+export default AddProduct;
